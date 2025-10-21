ad83c7d31c1c38e9d79f426cc561155ca59d29ba
peasfultown <peasfultown@gmail.com>
2025-10-21 16:50:53 +0700

    ref: applied title color patch

--- config.def.h
+++ config.def.h
@@ -20,9 +20,10 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*                  fg         bg         border   */
+	[SchemeNorm]    = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]     = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeTitle]   = { col_gray4, col_cyan,  col_cyan  },
 };
 
 static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
