d215ca0ecef7caa67dc8fc1f06c11e6c15e16fc0
PeasfulTown <peasfultown@gmail.com>
2026-01-17 16:07:16 +0700

    feat: added modus-vivendi theme

---
+++ themes/modus-vivendi.h
@@ -0,0 +1,22 @@
+static const char col_fg_norm[]      = "#ffffff";
+static const char col_bg_norm[]      = "#000000";
+static const char col_border_norm[]  = "#000000";
+
+static const char col_fg_sel[]       = "#ffffff";
+static const char col_bg_sel[]       = "#2f3849";
+static const char col_border_sel[]   = "#c0965b";
+
+
+static const char col_fg_title[]     = "#ffffff";
+static const char col_bg_title[]     = "#2f3849";
+static const char col_border_title[] = "#000000";
+
+
+static const char *colors[][3]      = {
+	/*                  fg              bg                  border   */
+	[SchemeNorm]    = { col_fg_norm     , col_bg_norm       ,  col_border_norm },
+	[SchemeSel]     = { col_fg_sel      , col_bg_sel        ,  col_border_sel  },
+	[SchemeTitle]   = { col_fg_title    , col_bg_title      ,  col_border_title  },
+};
+
+
